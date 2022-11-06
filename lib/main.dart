10135368cc685f9ab3434093ee664aafb63c0722
Sangjun-man <molung@icloud.com>
2022-11-07 00:22:30 +0900

    feat : 웹뷰 컴포넌트 제작

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/components/web_view.dart';
 import 'package:todo_list/screen/home_page.dart';
 
 void main() {
@@ -12,11 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage());
   }
 }
